0041bfa04d0637ef21ab6ff238dc6b566a9a86a7
Manu200162 <manli200134@gmail.com>
2021-12-06 20:37:49 -0400

    ultima modificacion

--- src/models/reservacion.model.js
+++ src/models/reservacion.model.js
@@ -13,11 +13,9 @@ const BoletaReservacion = function(boletareservacion) {
 };
 
 BoletaReservacion.create = (newBoletaReservacion, result) => {
-    console.log(newBoletaReservacion)
     let query = "INSERT INTO boletareservacion (id_libro,id_usuario,fecha_reservacion,fecha_devolucion) values("
     query+=newBoletaReservacion.id_libro+","
     query+=newBoletaReservacion.id_usuario+" ,"
-    //query+=`str_to_date(${newBoletaReservacion.fecha_reservacion},'YYYY-MM-DD') , str_to_date(${newBoletaReservacion.fecha_devolucion},'YYYY-MM-DD') )`
     query+=newBoletaReservacion.fecha_reservacion+","+newBoletaReservacion.fecha_devolucion+")"
     sql.query(query, newBoletaReservacion, (err, res) => {
     if (err) {
@@ -25,6 +23,17 @@ BoletaReservacion.create = (newBoletaReservacion, result) => {
       result(err, null);
       return;
     }
+    sql.query(
+        "UPDATE libro SET reservado = true WHERE id_libro = ?",
+        [newBoletaReservacion.id_libro],
+        (err, resUpdate) => {
+          if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+          }
+        }
+    );
 
     console.log("created reservation form: ", { id: res.insertId, ...newBoletaReservacion });
     result(null, { id: res.insertId, ...newBoletaReservacion });
